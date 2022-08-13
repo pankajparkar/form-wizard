af4606b86e9f80abe3631c3f4dfe733390f50516
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-14 00:05:27 +0800

    User details value populated from dynamic objects.

--- src/app/components/user-details-form/user-details-form.component.ts
+++ src/app/components/user-details-form/user-details-form.component.ts
@@ -1,6 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// TODO: move to different files
+interface Location {
+  id: number;
+  name: string;
+  code: string;
+}
+
+// TODO: move to different files
+interface Package {
+  id: string;
+  name: string;
+  description: string;
+  percentage: number;
+}
 
 @Component({
   selector: 'fw-user-details-form',
@@ -13,18 +28,54 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
   templateUrl: './user-details-form.component.html',
   styleUrls: ['./user-details-form.component.scss']
 })
-export class UserDetailsFormComponent implements OnInit {
+export class UserDetailsFormComponent {
+
+  locations: Location[] = [
+    {
+      id: 1,
+      name: 'Hong Kong',
+      code: 'HKD',
+    },
+    {
+      id: 2,
+      name: 'USA',
+      code: 'USD',
+    },
+    {
+      id: 1,
+      name: 'Australia',
+      code: 'AUD',
+    },
+  ];
+
+  packages: Package[] = [
+    {
+      id: 'standard',
+      name: 'Standard',
+      description: 'Standard',
+      percentage: 1,
+    },
+    {
+      id: 'safe',
+      name: 'Safe',
+      description: 'Safe (+250HKD, 50%)',
+      percentage: 50,
+    },
+    {
+      id: 'super_safe',
+      name: 'Super Safe',
+      description: 'Super Safe (+375',
+      percentage: 50,
+    },
+  ]
 
   userDetailsForm = new FormGroup({
     name: new FormControl<string>('', [Validators.required]),
     age: new FormControl<string>('', [Validators.required]),
     location: new FormControl<string>('', [Validators.required]),
     package: new FormControl<string>('', [Validators.required]),
-  })
+  });
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
