4e2a467135e4176fc0ce87e264657ae70f4a5a0d
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-13 23:41:59 +0800

    Added view contents for summary and user-details-form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ErrorComponent } from './components/error/error.component';
 
 import { StepComponent } from './components/step/step.component';
 import { StepperComponent } from './components/stepper/stepper.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { UserDetailsFormComponent } from './components/user-details-form/user-details-form.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @Component({
@@ -15,6 +18,9 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     StepperComponent,
     StepComponent,
     WelcomeComponent,
+    UserDetailsFormComponent,
+    SummaryComponent,
+    ErrorComponent,
   ]
 })
 export class AppComponent {
--- src/app/components/user-details-form/user-details-form.component.ts
+++ src/app/components/user-details-form/user-details-form.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fw-user-details-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './user-details-form.component.html',
   styleUrls: ['./user-details-form.component.scss']
 })
 export class UserDetailsFormComponent implements OnInit {
+
+  userDetailsForm = new FormGroup({
+    name: new FormControl<string>('', [Validators.required]),
+    age: new FormControl<string>('', [Validators.required]),
+    location: new FormControl<string>('', [Validators.required]),
+    package: new FormControl<string>('', [Validators.required]),
+  })
 
   constructor() { }
 
