94aaab16ea19868902dc0de45c6784d8b6422537
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-13 15:43:56 +0800

    Used standalone components everywhere.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'fw-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'form-wizard';
