f28290444f2131c23c13eb85b3e2a783769489c6
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-15 04:33:07 +0800

    Summary component test is added.

--- src/app/components/summary/summary.component.spec.ts
+++ src/app/components/summary/summary.component.spec.ts
@@ -8,16 +8,32 @@ describe('SummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ SummaryComponent ]
+      imports: [SummaryComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SummaryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create a component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render user details when object is passed', () => {
+    component.userDetails = {
+      age: 10,
+      location: 'HKD',
+      name: 'Pankaj',
+      package: 'standard',
+      total: 100,
+    }
+
+    expect(fixture.debugElement.nativeElement.querySelector('.summary .user-details')).toBeDefined();
+  });
+
+  it('should not render user details when object is empty', () => {
     expect(component).toBeTruthy();
   });
 });
