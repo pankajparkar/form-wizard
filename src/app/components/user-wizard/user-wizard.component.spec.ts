e32d1a6832236b8d6baf781b2889f8e8ed19d69d
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-08-15 04:17:37 +0800

    complete user-wizard component tests.

--- src/app/components/user-wizard/user-wizard.component.spec.ts
+++ src/app/components/user-wizard/user-wizard.component.spec.ts
@@ -1,21 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { UserDetails } from '@models/user-details.model';
+import { FormWizardService } from '@services/form-wizard.service';
 
 import { UserWizardComponent } from './user-wizard.component';
 
-fdescribe('UserWizardComponent', () => {
+describe('UserWizardComponent', () => {
   let component: UserWizardComponent;
   let fixture: ComponentFixture<UserWizardComponent>;
+  const fakeService = {
+    submit: () => { },
+    getDetails: () => ({}),
+    setDetails: () => { },
+  }
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [UserWizardComponent]
+      imports: [
+        UserWizardComponent
+      ],
+      providers: [
+        { provide: FormWizardService, useValue: fakeService }
+      ],
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(UserWizardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+
   });
 
   it('should render with a stepper component', () => {
@@ -25,7 +41,9 @@ fdescribe('UserWizardComponent', () => {
 
   describe('ErroChecker', () => {
     it('should return true if age is greater', () => {
-      expect(component.errorChecker({ age: 10, location: '1', name: '1', package: '1', total: 100 })).toBeFalsy();
+      expect(component.errorChecker({
+        age: 10, location: '1', name: '1', package: '1', total: 100
+      })).toBeFalsy();
     });
 
     it('should return false if age is lesser', () => {
@@ -35,5 +53,43 @@ fdescribe('UserWizardComponent', () => {
       ).toBeTruthy();
     });
   });
+  it('should call formWizard and stepper methods on wizardFinish', () => {
+    // arrange
+    const spyOnFormWizardSubmit = spyOn(fakeService, 'submit');
+    const spyOnUpdateIndex = spyOn(component.stepper, 'updateIndex');
+    const spyOnClear = spyOn(component.stepper, 'clear');
 
+    // act
+    component.wizardFinish();
+
+    // assert
+    expect(spyOnFormWizardSubmit).toHaveBeenCalled();
+    expect(spyOnUpdateIndex).toHaveBeenCalled();
+    expect(spyOnClear).toHaveBeenCalled();
+  });
+
+
+  describe('nextClick', () => {
+    it('should call formWizard setDetails when contains formData', () => {
+      // arrange
+      const spyOnFormWizardSetDetails = spyOn(fakeService, 'setDetails');
+
+      // act
+      component.nextClick({});
+
+      // assert
+      expect(spyOnFormWizardSetDetails).toHaveBeenCalled();
+    });
+
+    it('should not call formWizard setDetails when contains formData', () => {
+      // arrange
+      const spyOnFormWizardSetDetails = spyOn(fakeService, 'setDetails');
+
+      // act
+      component.nextClick(null);
+
+      // assert
+      expect(spyOnFormWizardSetDetails).not.toHaveBeenCalled();
+    });
+  });
 });
